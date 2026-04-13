331f277ccf20101110114054b61c154a38486edc
fabianTholin <thfa25ko@student.ju.se>
2026-04-13 11:44:20 +0200

    react-calendar på plats

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css'
-
-const App = ()=> {
+import "./App.css";
+import Calendar from "react-calendar";
+const App = () => {
   return (
     <>
-    <h1>Välkommen till Single page application Spa't!</h1>
+      <h1>Välkommen till Single page application Spa't</h1>
+      <Calendar></Calendar>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
